178b899a0b009c99805baf24244a7d127874b205
dyuthi92 <dyuthi.reddy@gmail.com>
2016-06-02 08:06:07 -0700

    Update Patientservlet.java
    Moved login method to doPost.

--- src/com/sainath/hospital/jdbc/Patientservlet.java
+++ src/com/sainath/hospital/jdbc/Patientservlet.java
@@ -210,8 +210,13 @@ public class Patientservlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	     try {
+					login(request,response);
+					
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 	}
 
 }
